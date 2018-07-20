8695d3c8fb3cccdff6550a73e73b0d5276a187d3
allgamecoin-dev <sun_stone@outlook.com>
2018-07-20 22:59:59 +0800

    add config

---
+++ agc-autominer/bll/Config.cs
@@ -0,0 +1,43 @@
+﻿using miner.common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace miner.bll
+{
+    public class Config
+    {
+        private static string iniFilePath = System.Windows.Forms.Application.StartupPath +  "\\config.ini";
+
+        public static bool configStartUp
+        {
+            get { return WinOSHelper.getStartUpState(); }
+            set { WinOSHelper.setStartUp(value); }
+        }
+        public static string configAccount
+        {
+            get { return IniHelper.INIGetStringValue(iniFilePath, "CONFIG", "ACCOUNT", ""); }
+            set { IniHelper.INIWriteValue(iniFilePath, "CONFIG", "ACCOUNT", value); }
+        }
+        public static string configPool
+        {
+            get { return IniHelper.INIGetStringValue(iniFilePath, "CONFIG", "POOL", ""); }
+            set { IniHelper.INIWriteValue(iniFilePath, "CONFIG", "POOL", value); }
+        }
+        public static int configGPUType
+        {
+            get
+            {
+                int index;
+                int.TryParse(IniHelper.INIGetStringValue(iniFilePath, "CONFIG", "GPUTYPE", ""), out index);
+                return index;
+            }
+            set
+            {
+                IniHelper.INIWriteValue(iniFilePath, "CONFIG", "GPUTYPE", value.ToString());
+            }
+        }
+    }
+}

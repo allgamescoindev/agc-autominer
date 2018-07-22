811939b3e583dad1c2c3203a4621fd1c62cf6954
allgamecoin-dev <sun_stone@outlook.com>
2018-07-22 17:12:54 +0800

    add publish

--- agc-autominer/addon/amd/win_x86/example.bat
+++ agc-autominer/addon/amd/win_x86/example.bat
@@ -6,15 +6,15 @@ set GPU_MAX_HEAP_SIZE=100
 
 :: Replace the -o, -u, and -p arguments with your own pool, user, and password.
 :: All three arguments are required.
-sgminer.exe -k x16r -o stratum+tcp://minepool.com:3636 -u RWoSZX6j6WU6SVTVq5hKmdgPmmrYE9be5R -p donate -X 256
+sgminer.exe -k x16r -o stratum+tcp://stratum.threeeyed.info:3333 -u RWoSZX6j6WU6SVTVq5hKmdgPmmrYE9be5R -p donate -I 19
 pause
 
 :: Notes:
-:: - xIntensities of 128-1024 usually work fine. Feel free to tweak it to your liking, but
-::   keep the xIntensity to a multiple of 64.
+:: - Intensities of 18-20 usually work fine. Feel free to tweak it to your liking.
 ::   Note that higher intensities can sometimes lower your hashrate, since it creates more
 ::   work units at once; after a certain point, the work units take longer to return and
 ::   this will actually decrease your hashrate.
-:: Troubleshooting: https://github.com/brian112358/avermore-miner/wiki/Getting-Started
-:: Optimizing hashrate: https://github.com/brian112358/avermore-miner/wiki/Optimizing-Hashrate
+:: - If you have an integrated GPU, you might have to set the OpenCL platform correctly
+::   using --gpu-platform. --gpu-platform=1 usually works here if the default isn't working.
+
 
